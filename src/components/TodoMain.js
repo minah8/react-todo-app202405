d5e8e0a7e5cfa6d05dc23a768342f91caf9e05d1
minah8 <o_uo_o@naver.com>
2024-05-17 12:12:23 +0900

    todo api와 todo app 연동

--- src/components/TodoMain.js
+++ src/components/TodoMain.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../scss/TodoMain.scss';
 import TodoItem from './TodoItem';
 
-const TodoMain = ({ todoList, remove, check, count }) => {
+const TodoMain = ({ todoList, remove, check }) => {
   return (
     <ul className="todo-list">
       {todoList.map((todo) => {
